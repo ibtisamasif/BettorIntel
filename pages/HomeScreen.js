246b548075408c1cf10864c430ad4e96ac4179f7
ranasami <ranasami006@gmail.com>
2019-11-15 03:00:21 +0500

    flex layout  experiment

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,11 +1,65 @@
 import React from 'react';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
- 
+
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ marginTop: 50, fontSize: 25 }}>Home!</Text>
+      <View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={{ marginTop: 50, fontSize: 25 }}>Home!</Text>
+        </View>
+        <View >
+          <View style={styles.container}>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+          </View>
+
+          <View style={styles.container2}>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+          </View>
+
+          <View style={styles.container}>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+          </View>
+
+          <View style={styles.container}>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+          </View>
+
+          <View style={styles.container}>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+          </View>
+
+          <View style={styles.container}>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+          </View>
+
+          <View style={styles.container}>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+          </View>
+
+          <View style={styles.container}>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+          </View>
+
+          <View style={styles.container}>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+
+          </View>
+          <View style={styles.container}>
+            <View style={styles.welcome}></View>
+            <View style={styles.welcome}></View>
+          </View>
+        </View>
       </View>
     );
   }
@@ -17,5 +71,23 @@ const styles = StyleSheet.create({
     padding: 10,
     width: 300,
     marginTop: 16,
+  },
+  container: {
+    borderWidth: 5,
+    flex: 1,
+    marginTop: 70,
+    marginRight: 20,
+    marginLeft: 20,
+    backgroundColor: '#F5FCFF',
+    flexDirection: 'row',
+    alignItems: "center",
+  },
+  container2: {
+    flex: 1,
+    //marginTop: 10,
+    marginRight: 20,
+    marginLeft: 20,
+    backgroundColor: '#F5FCFF',
+    flexDirection: 'row',
   },
 });
