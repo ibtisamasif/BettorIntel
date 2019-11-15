4b3fcd8f781ab198d4bdfd068c06867f912c40ca
ranasami <ranasami006@gmail.com>
2019-11-16 01:02:55 +0500

    DataTable

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,64 +1,87 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
-import { Table, TableWrapper,Col, Cols, Cell } from 'react-native-table-component';
+import { DataTable } from 'react-native-paper';
+
 
 export default class HomeScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    const elementButton = (value) => (
-      <TouchableOpacity onPress={() => this._alertIndex(value)}>
-        <View style={styles.btn}>
-          <Text style={styles.btnText}>button</Text>
-        </View>
-      </TouchableOpacity>
-    );
- 
-    this.state = {
-      tableTitle: ['Title', 'Title2', 'Title3', 'Title4'],
-      tableData: [
-        [elementButton('1'), 'a', 'b', 'c', 'd'],
-        [elementButton('2'), '1', '2', '3', '4'],
-        [elementButton('3'), 'a', 'b', 'c', 'd']
-      ]
-    }
-  }
- 
-  _alertIndex(value) {
-    Alert.alert(`This is column ${value}`);
-  }
-
-
-  render(){
-    const state = this.state;
+  render() {
     return (
-      <View style={styles.container}>
-        <Table style={{flexDirection: 'row'}} borderStyle={{borderWidth: 1}}>
-          {/* Left Wrapper */}
-          <TableWrapper style={{width: 80}}>
-            <Cell data="" style={styles.singleHead}/>
-            <TableWrapper style={{flexDirection: 'row'}}>
-              <Col data={['H1', 'H2']} style={styles.head} heightArr={[60, 60]} textStyle={styles.text} />
-              <Col data={state.tableTitle} style={styles.title} heightArr={[30, 30, 30, 30]} textStyle={styles.titleText}></Col>
-            </TableWrapper>
-          </TableWrapper>
- 
-          {/* Right Wrapper */}
-          <TableWrapper style={{flex:1}}>
-            <Cols data={state.tableData} heightArr={[40, 30, 30, 30, 30]} textStyle={styles.text}/>
-          </TableWrapper>
-        </Table>
+      <View style={styles.datatable }>      
+      <DataTable >
+        <DataTable.Header>
+          <DataTable.Title>Dessert</DataTable.Title>
+          <DataTable.Title >Calories</DataTable.Title>
+          <DataTable.Title numeric>Fat</DataTable.Title>
+        </DataTable.Header>
+
+        <DataTable.Row style={styles.datarow}>
+          <DataTable.Cell  style={styles.namecell}>Frozen yogurt</DataTable.Cell>
+          <DataTable.Cell style={styles.datacell}>159</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+        </DataTable.Row>
+
+        <DataTable.Row>
+          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+        </DataTable.Row>
+        <DataTable.Row>
+          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
+          <DataTable.Cell numeric>237</DataTable.Cell>
+        </DataTable.Row> 
+      </DataTable>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  singleHead: { width: 80, height: 40, backgroundColor: '#c8e1ff' },
-  head: { flex: 1, backgroundColor: '#c8e1ff' },
-  title: { flex: 2, backgroundColor: '#f6f8fa' },
-  titleText: { marginRight: 6, textAlign:'right' },
-  text: { textAlign: 'center' },
-  btn: { width: 58, height: 18, marginLeft: 15, backgroundColor: '#c8e1ff', borderRadius: 2 },
-  btnText: { textAlign: 'center' }
+  datacell:
+  {
+    padding:0,
+  },
+  namecell:{
+   borderColor:'black',
+   borderEndWidth:2, 
+  },
+  datatable:{
+    borderWidth:5,
+    borderColor:'black',
+    padding:20,
+    margin:20,
+
+},
+datarow:{
+borderBottomWidth:2,
+borderBottomColor:'black',
+}
+
 
 });
