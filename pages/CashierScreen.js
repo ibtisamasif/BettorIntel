e9bc3b3092bffa201e9ca11bb83ffc4b1c89cf9e
ranasami <ranasami006@gmail.com>
2019-11-17 03:35:11 +0500

    cashier screen basic

--- pages/CashierScreen.js
+++ pages/CashierScreen.js
@@ -1,21 +1,49 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Alert,Text, View, TouchableOpacity, StyleSheet } from 'react-native';
  
 export default class CashierScreen extends React.Component {
+  _onPressButton() {  
+    Alert.alert('You tapped the button!')  
+}  
+
+_onLongPressButton() {  
+    Alert.alert('You long-pressed the button!')  
+}  
+  
+  
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ marginTop: 50, fontSize: 25 }}>Cashier!</Text>
+        <TouchableOpacity onPress={this._onPressButton} onLongPress={this._onLongPressButton}>  
+                    <View style={styles.button}>  
+                        <Text style={styles.buttonText}>TouchableOpacity</Text>  
+                    </View>  
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this._onPressButton} onLongPress={this._onLongPressButton}>  
+                    <View style={styles.button}>  
+                        <Text style={styles.buttonText}>TouchableOpacity</Text>  
+                    </View>  
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this._onPressButton} onLongPress={this._onLongPressButton}>  
+                    <View style={styles.button}>  
+                        <Text style={styles.buttonText}>TouchableOpacity</Text>  
+                    </View>  
+                </TouchableOpacity>
+                
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    width: 300,
-    marginTop: 16,
-  },
+  button: {  
+    marginBottom: 30,  
+    width: 260,  
+    alignItems: 'center',  
+    backgroundColor: '#5ead97'  
+},  
+buttonText: {  
+    padding: 20,  
+    color: 'white',  
+    fontSize: 18 
+  } 
 });
