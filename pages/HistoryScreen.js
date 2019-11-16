9111c1b23e3c5d1a596c968e135b2edef7e0c2b6
ranasami <ranasami006@gmail.com>
2019-11-17 01:00:39 +0500

    table and graph added

--- pages/HistoryScreen.js
+++ pages/HistoryScreen.js
@@ -1,38 +1,82 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { LineChart } from 'react-native-line-chart'
+import { Table, Row} from 'react-native-table-component';
 
 export default class HistoryScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['Head', 'Head2', 'Head3'],
+      tableData: [
+        ['1', '2', '3'],
+        ['a', 'b', 'c'],
+        ['1', '2', '3'],
+        ['a', 'b', 'c']
+      ]
+    }
+  }
+  
+  
   render() {
+    const state = this.state;
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ marginTop: 50, fontSize: 25 }}>History!</Text>
-      </View>
+  <View>
+      <View>
+  <Text>
+    Bezier Line Chart
+  </Text>
+  <LineChart
+    data={{
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [{
+        data: [
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100,
+          Math.random() * 100
+        ]
+      }]
+    }}
+    width={Dimensions.get('window').width} // from react-native
+    height={220}
+    chartConfig={{
+      backgroundColor: '#e26a00',
+      backgroundGradientFrom: '#fb8c00',
+      backgroundGradientTo: '#ffa726',
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
+</View>
+
+<View style={styles.container}>
+        <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+          <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
+          <Row data={state.tableData} textStyle={styles.text}/>
+        </Table>
+  </View>
+</View>
+
     );
   }
+
+
+  
 }
 const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    width: 300,
-    marginTop: 16,
-  },
-  container: {
-    borderWidth: 5,
-    flex: 1,
-    marginTop: 70,
-    marginRight: 20,
-    marginLeft: 20,
-    backgroundColor: '#F5FCFF',
-    flexDirection: 'row',
-    alignItems: "center",
-  },
-  container2: {
-    flex: 1,
-    marginRight: 20,
-    marginLeft: 20,
-    backgroundColor: '#F5FCFF',
-    flexDirection: 'row',
-  },
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6 }
+
 });
--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,87 +1,59 @@
+//import React, { Component } from 'react';
+//import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
+//import { DataTable } from 'react-native-paper';
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
-import { DataTable } from 'react-native-paper';
-
-
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { Table, TableWrapper, Row} from 'react-native-table-component';
 export default class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.datatable }>      
-      <DataTable >
-        <DataTable.Header>
-          <DataTable.Title>Dessert</DataTable.Title>
-          <DataTable.Title >Calories</DataTable.Title>
-          <DataTable.Title numeric>Fat</DataTable.Title>
-        </DataTable.Header>
-
-        <DataTable.Row style={styles.datarow}>
-          <DataTable.Cell  style={styles.namecell}>Frozen yogurt</DataTable.Cell>
-          <DataTable.Cell style={styles.datacell}>159</DataTable.Cell>
-        </DataTable.Row>
-
-        <DataTable.Row>
-          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-          <DataTable.Cell numeric>237</DataTable.Cell>
-        </DataTable.Row>
-
-        <DataTable.Row>
-          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-          <DataTable.Cell numeric>237</DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row>
-          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-          <DataTable.Cell numeric>237</DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row>
-          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-          <DataTable.Cell numeric>237</DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row>
-          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-          <DataTable.Cell numeric>237</DataTable.Cell>
-          <DataTable.Cell numeric>237</DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row>
-          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-          <DataTable.Cell numeric>237</DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row>
-          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-          <DataTable.Cell numeric>237</DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row>
-          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-          <DataTable.Cell numeric>237</DataTable.Cell>
-        </DataTable.Row>
-        <DataTable.Row>
-          <DataTable.Cell>Ice cream sandwich</DataTable.Cell>
-          <DataTable.Cell numeric>237</DataTable.Cell>
-        </DataTable.Row> 
-      </DataTable>
-      </View>
-    );
+  
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['Head', 'Head2', 'Head3'],
+      tableData: [
+        ['DAL VS PHI'],
+        ['importal Picks'],
+        ['importal Picks'],
+        
+      ],
+      widthArr: [160,165]
+      
+    }
   }
+ 
+  render() {
+    const state = this.state;
+    return (
+      <ScrollView>
+              <View style={styles.container}>
+        
+        <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+          <Row data={state.tableHead}  widthArr={[160,83,83]} style={styles.head} textStyle={styles.text} />
+          
+          <Row data={state.tableData}  widthArr={state.widthArr} textStyle={styles.text}/>
+          <Row data={state.tableData} widthArr={state.widthArr} textStyle={styles.text}/>
+          <Row data={state.tableData} widthArr={state.widthArr} textStyle={styles.text}/>
+          <Row data={state.tableData} widthArr={state.widthArr} textStyle={styles.text}/>
+          <Row data={[['DAL VS PH'],['SF-1'],['1']]} widthArr={[160,83,82]} textStyle={styles.text}/>
+          <Row data={state.tableData} widthArr={state.widthArr} textStyle={styles.text}/>
+          <Row data={state.tableData} widthArr={state.widthArr} textStyle={styles.text}/>
+          <Row data={state.tableData} widthArr={state.widthArr} textStyle={styles.text}/>
+          <Row data={state.tableData} widthArr={state.widthArr} textStyle={styles.text}/>
+          
+        </Table>
+        
+      </View>
+      </ScrollView>
+    )
+  }
+
 }
+
 const styles = StyleSheet.create({
-  datacell:
-  {
-    padding:0,
-  },
-  namecell:{
-   borderColor:'black',
-   borderEndWidth:2, 
-  },
-  datatable:{
-    borderWidth:5,
-    borderColor:'black',
-    padding:20,
-    margin:20,
 
-},
-datarow:{
-borderBottomWidth:2,
-borderBottomColor:'black',
-}
-
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6, height:40}
 
 });
