fc1a4c8ac390d01cd6a25081d7fedc0b7ae72358
Ibtisam Asif <ibtisamasif@gmail.com>
2019-11-21 05:47:27 +0500

    indentation

--- pages/components/NavHeade.js
+++ pages/components/NavHeade.js
@@ -1,56 +1,55 @@
 import React from 'react';
-import {Text, View, StyleSheet,Image} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 
 export default class NavHead extends React.Component {
-  
   render() {
     return (
-        <View style={styles.containernav}>
-           <View style={styles.navBar}>
+      <View style={styles.containernav}>
+        <View style={styles.navBar}>
           <Text style={styles.navBarButton}>IMMORTAL</Text>
-          <Image style={styles.navBarHeader} source={require('../../image/Webp.net-resizeimage.png')} />
-       <Text style={styles.navBarButton}>PICKS</Text>
+          <Image
+            style={styles.navBarHeader}
+            source={require('../../image/Webp.net-resizeimage.png')}
+          />
+          <Text style={styles.navBarButton}>PICKS</Text>
         </View>
       </View>
-    )};
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-  
-  containernav:{
-    marginTop:5,
-},
- navBarButton: {
-   color: '#FACF0C',
-   textAlign: 'center',
-   
-   width: 85,
-   marginLeft: 10,
-   fontWeight: 'bold',
-   fontSize: 16,
-   fontFamily:'ariel',
-   marginBottom: 10,
- },
- navBarHeader: {
-   flex: 1,
-  marginLeft:55,
-  marginTop:20,
-  marginRight:40,
-  //padding:55,
-marginBottom: 5,
- // height:10,
- },
- navBar: {
-   flexDirection: 'row',
-   
-   paddingTop: 10,
-   height: 64,
-   backgroundColor: 'black',
-   borderWidth: 2,
-   //borderColor: 'gold',
-   alignItems: 'center',
- },
-  
-  });
-  
+  containernav: {
+    marginTop: 5,
+  },
+  navBarButton: {
+    color: '#FACF0C',
+    textAlign: 'center',
+
+    width: 85,
+    marginLeft: 10,
+    fontWeight: 'bold',
+    fontSize: 16,
+    fontFamily: 'ariel',
+    marginBottom: 10,
+  },
+  navBarHeader: {
+    flex: 1,
+    marginLeft: 55,
+    marginTop: 20,
+    marginRight: 40,
+    //padding:55,
+    marginBottom: 5,
+    // height:10,
+  },
+  navBar: {
+    flexDirection: 'row',
+
+    paddingTop: 10,
+    height: 64,
+    backgroundColor: 'black',
+    borderWidth: 2,
+    //borderColor: 'gold',
+    alignItems: 'center',
+  },
+});
